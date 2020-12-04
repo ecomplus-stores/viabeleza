6fbdf3932597735794e4ccfa2ee963b92304a0b0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-12-03 21:25:21 -0300

    chore: layout update

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
+$(".rastrear").click(function(i) {
+  i.preventDefault();
+  var t = "http://rastreie.com/" + $(".form-rastreio input").val();
+  window.open(t, "blank")
+})
